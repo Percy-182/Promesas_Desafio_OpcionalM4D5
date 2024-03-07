30bcf20b1cd5bdd43e1fd39481e7dff192125f07
Percy_182 <percy.ignacio.garcia@gmail.com>
2024-03-07 17:17:31 -0300

    "comentado algunas lineas"

--- Desafio_Promesas.js
+++ Desafio_Promesas.js
@@ -3,9 +3,11 @@
 // Luego mediante el bloque de try/catch conectarse a la URL indicada anteriormente con el método fetch, utilizando a la vez await para que retorne directamente el valor de la promesa
 
 const obtenerDatos = async () => {
+  // Se declara una constante llamada obtenerDatos que contiene una función asíncrona.
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/photos"); //solicitud GET
     if (!response.ok) {
+      //Se verifica si la respuesta de la solicitud fue exitosa. Si no lo fue, se lanza un error.
       throw new Error("Error en la solicitud: " + response.status);
     }
     const data = await response.json();
@@ -19,17 +21,18 @@ const obtenerDatos = async () => {
   } catch (error) {
     console.error("Error:", error.message);
   }
-};
+}; //cierre bloque
 
 const obtenerMensaje = () => {
   // se declara constante con tiempo seteado y devuelva un msj
   return new Promise((resolve, reject) => {
     //
     setTimeout(() => {
-      resolve("Información Enviada");
+      //Se utiliza setTimeout para setear/ejecutar la función después de un cierto tiempo (en este caso, 3000 milisegundos/3 segundos).
+      resolve("Información Enviada"); //Se llama a la función resolve de la promesa, lo que significa que la promesa se resuelve con el mensaje "Información Enviada" después de 3 segundos.
     }, 3000);
   });
-};
+}; //cierre bloque
 
 const recibirMensaje = async () => {
   try {
@@ -38,6 +41,6 @@ const recibirMensaje = async () => {
   } catch (error) {
     console.error("Error al recibir el mensaje:", error);
   }
-};
+}; //cierre bloque
 
 obtenerDatos();
